4ab56d61966777d0fd5ca48443fc54bd2946d1de
cristian-gabbanini <cristian.gabbanini@gmail.com>
2015-08-13 11:33:35 +0200

    Aggiunta funzione remove

--- ZSESwitch.js
+++ ZSESwitch.js
@@ -24,7 +24,11 @@
             }
             
             return obj;
-        };               
+        };
+        
+        this.remove = function() {
+            $(this.element).remove();
+        };
         
     };
                                       
